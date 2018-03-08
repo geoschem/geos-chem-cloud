1ea9f1e3e09b7d07e54f78a50e8dffce83e11e4c
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-03-07 21:19:28 -0500

    Install older version of tornado to prevent Jupyter error

--- build_scripts/python/basic_packages.sh
+++ build_scripts/python/basic_packages.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # absolutely minimum requirement to analyze NetCDF data
-conda install -y jupyter netcdf4 xarray cartopy
+# NOTE: Install older version of tornado to prevent Jupyter error
+# https://stackoverflow.com/questions/49141525/install-jupyter-notebook-on-miniconda
+conda install -y tornado=4.5.3 jupyter netcdf4 xarray cartopy
 
-# Also install AWSCLI into conda environment
+# Also install AWSCLI into conda root environment
 pip install -y awscli
