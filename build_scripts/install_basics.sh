e3b7427b3adeb91e68512ba33d8f673bfcc7cf49
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-03-16 18:50:06 -0400

    add emacs installation command

--- build_scripts/install_basics.sh
+++ build_scripts/install_basics.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# The quickest way to install NetCDF libraries. Can compile GEOS-Chem classic.
 # Tested on EC2 ubuntu-xenial-16.04 (ami-80861296)
 
 sudo apt-get update
+
+# The quickest way to install NetCDF libraries. Can compile GEOS-Chem classic.
 sudo apt-get install -y \
     bc gcc gfortran \
     libnetcdf-dev libnetcdff-dev netcdf-bin
+
+# Ubuntu AMI has vim and nano but no emacs
+sudo apt-get install -y emacs
