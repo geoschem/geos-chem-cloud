c73cfd0537b9706111f7929abb6b355422db8ab6
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-08-19 22:00:47 -0400

    RE-organize build scripts into multiple folders
    - GC-classic
    - GCHP
    - python
    - container

---
+++ build_scripts/build_environment/Container/install_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Install Docker-CE on AWS Ubuntu VM
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+# This is useful for building CharlieCloud images
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install -y docker-ce
+
+sudo docker run --rm hello-world
