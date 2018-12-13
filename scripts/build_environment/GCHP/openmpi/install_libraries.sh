42437d7a0bc227fecec63f41fae9edb9254084c9
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2018-12-12 19:27:22 -0500

    Updrade to OpenMPI3 to fix diagnostics error
    See https://github.com/geoschem/gchp/issues/10#issuecomment-446775489

--- scripts/build_environment/GCHP/openmpi/install_libraries.sh
+++ scripts/build_environment/GCHP/openmpi/install_libraries.sh
@@ -6,10 +6,9 @@
 # =========================
 sudo apt-get update
 
-# All libraries need for GCHP. f2py and mpich are new libraries in addition to GC-classic.
 sudo apt-get install -y bc gcc gfortran \
     libnetcdf-dev libnetcdff-dev netcdf-bin \
-    python-numpy libopenmpi-dev
+    python-numpy
 
 # Ubuntu AMI has vim and nano but no emacs
 sudo apt-get install -y emacs
@@ -20,6 +19,25 @@ sudo apt-get install -y awscli
 # clean up cache
 sudo apt-get clean
 sudo rm -rf /var/lib/apt/lists/*
+
+# =========================
+# OpenMPI 3
+# =========================
+# https://www.open-mpi.org/software/ompi/v3.1/
+wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.3.tar.gz
+tar zxf openmpi-3.1.3.tar.gz
+cd openmpi-3.1.3
+./configure prefix=/usr/local/
+make -j4
+sudo make install
+
+# Fix library linking
+# https://askubuntu.com/a/1100000
+sudo ldconfig
+
+cd ..
+rm -rf openmpi-3.1.3
+# =========================
 
 # =========================
 # Additional fixes for GCHP
