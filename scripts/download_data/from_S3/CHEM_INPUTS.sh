ad7db2a7546402d8819859fe8c2db9a376f5fdb4
JiaweiZhuang <jiaweizhuang@g.harvard.edu>
2019-01-27 15:26:15 -0500

    Several CHEM_INPUTS/ subdirectories have changed

--- scripts/download_data/from_S3/CHEM_INPUTS.sh
+++ scripts/download_data/from_S3/CHEM_INPUTS.sh
@@ -7,18 +7,16 @@ else
   exit 1
 fi
 
-time aws s3 cp --request-payer=requester --recursive \
-s3://gcgrid/GEOS_NATIVE $DATA_ROOT/CHEM_INPUTS  \
---exclude "*" \
---include "FastJ_201204/*" \
---include "Linoz_200910/*" \
---include "MODIS_LAI_201204/*" \
---include "Olson_Land_Map_201203/*" \
---include "TOMAS_201402/*" \
---include "UCX_201403/*"
+
 
 aws s3 cp --request-payer=requester --recursive \
 s3://gcgrid/CHEM_INPUTS $DATA_ROOT/CHEM_INPUTS  \
 --exclude "*" \
 --include "MODIS_LAI_201707/*" \
 --include "FAST_JX/*"
+--include "FastJ_201204/*" \
+--include "Linoz_200910/*" \
+--include "MODIS_LAI_201204/*" \
+--include "Olson_Land_Map_201203/*" \
+--include "TOMAS_201402/*" \
+--include "UCX_201403/*"
